1a811403a3581c023719ca40f8366810fd76e96c
James Gundlach <james.gundlach79@gmail.com>
2023-08-04 18:46:17 -0400

    Update install_webcord.sh

--- install_webcord.sh
+++ install_webcord.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
 
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt update
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/SpacingBat3/WebCord/releases/download/v4.2.0/webcord_4.2.0_arm64.deb
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0  sudo -S apt install ./webcord_4.2.0_arm64.deb -y
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 rm webcord_4.2.0_arm64.deb
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 apt update
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/SpacingBat3/WebCord/releases/download/v4.2.0/webcord_4.2.0_arm64.deb
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install ./webcord_4.2.0_arm64.deb -y
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 rm webcord_4.2.0_arm64.deb
 
 echo "[Desktop Entry]
 Name=Discord
