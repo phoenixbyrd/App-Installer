0c8f15b845a38be808ba6c161a322720028193e3
James Gundlach <james.gundlach79@gmail.com>
2023-08-04 20:35:44 -0400

    Update install_notion.sh

--- install_notion.sh
+++ install_notion.sh
@@ -4,12 +4,12 @@ varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/hom
 
 #Install Obsidian
 
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 sudo apt install zlib1g-dev
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0  wget https://github.com/notion-enhancer/notion-repackaged/releases/download/v2.0.18-1/Notion-2.0.18-1-arm64.AppImage
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0  chmod +x Notion-2.0.18-1-arm64.AppImage
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0  ./Notion-2.0.18-1-arm64.AppImage --appimage-extract
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0  mv squashfs-root notion
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0  rm Notion-2.0.18-1-arm64.AppImage
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install zlib1g-dev -y
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/notion-enhancer/notion-repackaged/releases/download/v2.0.18-1/Notion-2.0.18-1-arm64.AppImage
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 chmod +x Notion-2.0.18-1-arm64.AppImage
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 ./Notion-2.0.18-1-arm64.AppImage --appimage-extract
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 mv squashfs-root notion
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 rm Notion-2.0.18-1-arm64.AppImage
 
 #Create Desktop Launcher
 
