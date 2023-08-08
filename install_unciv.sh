97c74ef4a2be79d872da8ae1044cec92f58ba333
James Gundlach <james.gundlach79@gmail.com>
2023-08-08 06:16:37 -0400

    Add files via upload

---
+++ install_unciv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
+
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install default-jdk
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/yairm210/Unciv/releases/download/4.7.11/Unciv.jar
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 mv Unciv.jar /usr/games
+
+# Create the desktop entry
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Unciv
+Comment=
+Exec=proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 java -jar /usr/games/Unciv.jar
+Icon=unciv
+Categories=Game;
+Path=
+Terminal=false
+StartupNotify=true
+" > $HOME/Desktop/unciv.desktop
+
+chmod +x $HOME/Desktop/unciv.desktop
+cp $HOME/Desktop/unciv.desktop $HOME/../usr/share/applications/unciv.desktop
