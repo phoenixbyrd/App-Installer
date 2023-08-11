6145f51d1df07210ae499a0a1a62b8e691718f25
James Gundlach <james.gundlach79@gmail.com>
2023-08-11 19:41:04 -0400

    Update install_unciv.sh

--- install_unciv.sh
+++ install_unciv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
 
-proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install default-jdk
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install openjdk-17-jre
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/yairm210/Unciv/releases/download/4.7.11/Unciv.jar
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 mv Unciv.jar /usr/games
 
