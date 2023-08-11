26fbdf1f0a2a8bcd10f3b4765f8410b30376b67f
James Gundlach <james.gundlach79@gmail.com>
2023-08-11 19:40:41 -0400

    Update install_shatteredpd.sh

--- install_shatteredpd.sh
+++ install_shatteredpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
 
-proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install default-jdk
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install openjdk-17-jre
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 wget -O ShatteredPD.jar https://github.com/00-Evan/shattered-pixel-dungeon/releases/download/v2.1.4/ShatteredPD-v2.1.4-Java.jar
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 mkdir -p /opt/ShatteredPD
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 mv ShatteredPD.jar /opt/ShatteredPD/
