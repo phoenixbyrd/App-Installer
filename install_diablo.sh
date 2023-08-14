f655e91904f3c2dd41a486a40e04cde682dee48f
James Gundlach <james.gundlach79@gmail.com>
2023-08-14 09:57:43 -0400

    Update install_diablo.sh

--- install_diablo.sh
+++ install_diablo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
 
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install libsdl2-image-2.0-0 libsodium23
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/diasurgical/devilutionX/releases/download/1.5.0/devilutionx-linux-aarch64.tar.xz
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 mkdir -p devilutionx
 proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 tar -xvf devilutionx-linux-aarch64.tar.xz  -C devilutionx
