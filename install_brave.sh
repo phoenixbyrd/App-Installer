0ff1699f80aee2236c74626c2738a8382a319e67
James Gundlach <james.gundlach79@gmail.com>
2023-08-19 12:59:01 -0400

    Update install_brave.sh

--- install_brave.sh
+++ install_brave.sh
@@ -2,8 +2,10 @@
 
 varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
 
-proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/brave/brave-browser/releases/download/v1.59.28/brave-browser-nightly_1.59.28_arm64.deb
-proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install ./brave-browser-nightly_1.59.28_arm64.deb
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt update
+proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install brave-browser
 
 echo "[Desktop Entry]
 Version=1.0
