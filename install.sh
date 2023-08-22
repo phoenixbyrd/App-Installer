98699c7a8b978c57aede09009cff58ed91e2d15b
James Gundlach <james.gundlach79@gmail.com>
2023-08-22 08:19:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -841,7 +841,7 @@ choice=$(zenity --list --radiolist \
     FALSE "$simplenote_action" "$simplenote_description" \
     FALSE "$onepassword_action" "$onepassword_description" \
     SEPARATOR \
-    --width=800 --height=450)
+    --width=875 --height=450)
 
     # Check if the user canceled the selection
     if [ -z "$choice" ]; then
