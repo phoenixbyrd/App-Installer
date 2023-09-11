9f2fdfdce755cd5a9aa3b97a490e7f056514617f
James Gundlach <james.gundlach79@gmail.com>
2023-09-11 16:36:50 -0400

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -6,7 +6,7 @@ proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 wget https://packages
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install ./code.deb -y
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 rm code.deb
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install gpg software-properties-common apt-transport-https -y
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo apt-key add -
 #proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo -S install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 #proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo -S sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 #proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 rm packages.microsoft.gpg
