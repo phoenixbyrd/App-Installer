908e6113a0a1d3bb166050d95fac93ea5d4b78e0
phoenixbyrd <james.gundlach79@gmail.com>
2024-04-10 05:14:51 -0400

    Update install_obsidian.sh

--- install_obsidian.sh
+++ install_obsidian.sh
@@ -5,11 +5,11 @@ varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/hom
 #Install Obsidian
 
 proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install zlib1g-dev -y
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.5/Obsidian-1.3.5-arm64.AppImage
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 chmod +x Obsidian-1.3.5-arm64.AppImage
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 ./Obsidian-1.3.5-arm64.AppImage --appimage-extract
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 mv squashfs-root Obsidian
-proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 rm Obsidian-1.3.5-arm64.AppImage
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/Obsidian-1.5.12-arm64.AppImage
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 chmod +x Obsidian-1.5.12-arm64.AppImage
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 ./Obsidian-1.5.12-arm64.AppImage --appimage-extract
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 mv squashfs-root /opt/Obsidian
+proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 rm Obsidian-1.5.12-arm64.AppImage
 
 
 
@@ -18,7 +18,7 @@ proot-distro login --user $varname debian --shared-tmp -- env DISPLAY=:1.0 rm Ob
 echo "[Desktop Entry]
 Version=1.0
 Name=Obsidian
-Exec=proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 GALLIUM_DRIVER=virpipe Obsidian/./obsidian --no-sandbox
+Exec=proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 GALLIUM_DRIVER=virpipe /opt/Obsidian/./obsidian --no-sandbox
 StartupNotify=true
 Terminal=false
 Icon=obsidian
