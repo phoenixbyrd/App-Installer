c259a592415cfe9879f7882cc35b486898bb374d
phoenixbyrd <james.gundlach79@gmail.com>
2024-04-09 20:44:07 -0400

    Update install_gdlauncher.sh

--- install_gdlauncher.sh
+++ install_gdlauncher.sh
@@ -11,7 +11,7 @@ appname="gdlauncher"
 #/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian
 icon_path="gdlauncher"
 #Enter Categories for .desktop
-category="Games"
+category="Games;"
 #Enter any dependencies
 depends="mesa-utils"
 
